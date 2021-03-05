cdad2553f92efa29487aaff99a300950026fa6c8
Lufei Shuang <shuanglufei1994@gmail.com>
2021-03-05 07:09:18 +0700

    add d.ts under examples

--- examples/jsm/loaders/TTFLoader.d.ts
+++ examples/jsm/loaders/TTFLoader.d.ts
@@ -1,1 +1,15 @@
-export * from 'three/examples/jsm/loaders/TTFLoader'
+import { Loader, LoadingManager } from '../../../src/Three';
+
+export class TTFLoader extends Loader {
+    constructor(manager?: LoadingManager);
+    reversed: boolean;
+
+    load(
+        url: string,
+        onLoad: (json: object) => void,
+        onProgress?: (event: ProgressEvent) => void,
+        onError?: (event: ErrorEvent) => void,
+    ): void;
+    loadAsync(url: string, onProgress?: (event: ProgressEvent) => void): Promise<object>;
+    parse(arraybuffer: ArrayBuffer): object;
+}
